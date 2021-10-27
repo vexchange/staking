513380f1a7d30b65666805f3ae9837af0c5f198e
alexlau <alex.lau@proxima.capital>
2021-10-27 13:22:51 +0800

    Amended addreses of staking token and others

--- constants/index.js
+++ constants/index.js
@@ -4,18 +4,19 @@ export const NETWORK_NAMES = {
   1: 'mainnet',
   42: 'testnet',
 }
-
-export const VEX_ADDRESSES = {
-  testnet: '0x377b467867CE906D14ea2bA610ED7553FE1Cd459',
-  mainnet: '0x377b467867CE906D14ea2bA610ED7553FE1Cd459',
+export const STAKING_TOKEN_ADDRESSES = {
+  testnet: '0x000000000000000000000050726f746f74797065',
+  mainnet: ''
 }
 
+// This is the rewards token to be given out
+// In our case it is VEX
 export const REWARD_TOKEN_ADDRESSES = {
   testnet: '0x377b467867CE906D14ea2bA610ED7553FE1Cd459',
-  mainnet: '0x377b467867CE906D14ea2bA610ED7553FE1Cd459',
+  mainnet: '',
 }
 
 export const REWARDS_ADDRESSES = {
   testnet: '0xa4668d3d4ffc949df7c0f27e74f19f4ce376f6db',
-  mainnet: '0xa4668d3d4ffc949df7c0f27e74f19f4ce376f6db',
+  mainnet: '',
 }
