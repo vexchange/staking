c5549eb42255bda0ba1b1d6f3023eabfe9b4008f
alexlau <lauyy@umich.edu>
2021-11-09 10:09:49 +0800

    Plugged in mainnet addresses

--- constants/index.js
+++ constants/index.js
@@ -16,26 +16,20 @@ export const CHAIN_ID = {
 }
 export const STAKING_TOKEN_ADDRESSES = {
   // Fill in the actual V2Pair token address for VEX-VET
-  mainnet: '',
-
-  // Just a random ERC20/VIP180 token
-  // Get Alex to transfer you some
+  mainnet: '0x39cd888a1583498AD30E716625AE1a00ff51286D',
   testnet: '0x136f4e8CD2A5dda330EF1B47A102B25122bf066C',
 }
 
 // Multirewards contract address for the staking token address
 // defined above
 export const REWARDS_ADDRESSES = {
-  mainnet: '',
-
-  // This addressed is now correct, configured to work
-  // With the correct staking token and reward token
+  mainnet: '0x538f8890a383c44e59df4c7263d96ca8048da2c7',
   testnet: '0x03ec9c93ed8a0dd68a9a4ac868c06fae3eecad01',
 }
 
 // This is the rewards token to be given out
 // In our case it is VEX
 export const REWARD_TOKEN_ADDRESSES = {
-  mainnet: '',
+  mainnet: '0x0BD802635eb9cEB3fCBe60470D2857B86841aab6',
   testnet: '0x7e46cAd7eB7ebc587ac36c30fE705eD77a686f60',
 }
