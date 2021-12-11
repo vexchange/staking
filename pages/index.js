11dffde04c43c743795609603f92ecf6cca23de6
A.L <xenide@users.noreply.github.com>
2021-12-11 08:49:04 +0800

    changed page title to Farm

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>VEX/VET Yield Farming | Vexchange</title>
+        <title>Farm | Vexchange</title>
         <meta name="description" content="Vexchange yield farming aims to incentivize VEX liquidity, expand the voting power to those who miss out on the airdrop, and distribute the governance token to those who have the most skin in the game." />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>" />
       </Head>
