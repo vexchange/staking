eceee27a34460411d76d339d0fa3cf95331d6253
A.L <xenide@users.noreply.github.com>
2022-04-03 22:16:19 +0800

    feat: adding VeUSD-VET as a new pool (#18)

--- hooks/useOverview.js
+++ hooks/useOverview.js
@@ -1,9 +1,10 @@
 import { useAppContext } from "../context/app";
 import { useEffect, useState } from "react";
 import {
-  STAKING_POOLS,
+  CHAIN_ID,
+  STAKING_POOLS, VECHAIN_NETWORK,
 } from "../constants";
-import { getMidPrice } from "../utils";
+import { Fetcher } from "vexchange-sdk";
 
 const useOverview = () => {
   const [poolInfo, setPoolInfo] = useState(null);
@@ -15,13 +16,20 @@ const useOverview = () => {
     try {
       let poolInfo = [];
       STAKING_POOLS.map(async (stakingPool) => {
-        const result = await getMidPrice(
-          connex,
-          stakingPool.stakeAssetUrlPart.split("-")[0],
-          stakingPool.stakeAssetUrlPart.split("-")[1]
-        );
 
-        const pair = result.pair;
+        const [token0, token1] = await Promise.all([
+            Fetcher.fetchTokenData(CHAIN_ID[VECHAIN_NETWORK],
+            stakingPool.stakeAssetUrlPart.split("-")[0],
+            connex),
+            Fetcher.fetchTokenData(CHAIN_ID[VECHAIN_NETWORK],
+            stakingPool.stakeAssetUrlPart.split("-")[1],
+            connex)
+        ]);
+
+        const pair = await Fetcher.fetchPairData(
+            token0,
+            token1,
+            connex);
 
         poolInfo[stakingPool.id] = {
           pair,
